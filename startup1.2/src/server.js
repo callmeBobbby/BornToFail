c0e316f2b3608a4439a67005d2dffd65cb7f415a
callmeBobbby <prashant.singhofficial2003@gmail.com>
2023-09-24 12:21:59 +0530

    learned how to create relational data and push into db

--- startup1.2/src/server.js
+++ startup1.2/src/server.js
@@ -5,26 +5,42 @@ const prisma = new PrismaClient();
 async function main() {
     try {
         // Create a new user
+
         const user = await prisma.user.create({
             data: {
-                name: 'Rich',
-                email: 'hello@prisma.com',
+                name: 'Bobby',
+                email: 'bobby@prisma.com',
+                address: {
+                    street: '123 Main St',
+                    city: 'City 1',
+                    state: 'State 1',
+                    zip: '12345',
+                },
             },
         });
 
-       
         console.log(user);
         // Create a new post associated with the user
         const post = await prisma.post.create({
             data: {
-                title: 'My new post',
-                body: 'This is the content of my new post.',
-                slug: 'my-new-post',
+                slug: 'my-new-post2',
+                title: 'This is second Post',
+                body: 'This is the content of my new second post.',
                 authorId: user.id,
             },
         });
-
         console.log('Created post:', post);
+
+        const comment = await prisma.comment.create({
+            data: {
+                comment: 'this is a decent comment',
+                postId: post.id
+            },
+        });
+
+        console.log('Comment created:', comment);
+
+
     } catch (error) {
         console.error('Error:', error);
     } finally {
