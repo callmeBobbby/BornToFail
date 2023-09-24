e4355aa5760b7dbd7483cd59d364267522d44ae2
callmeBobbby <prashant.singhofficial2003@gmail.com>
2023-09-24 10:51:02 +0530

    added some post for data model

--- startup1.2/src/server.ts
+++ startup1.2/src/server.ts
@@ -1,17 +1,33 @@
-import {PrismaClient} from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main(){
+async function main() {
+    try {
+        // Create a new user
+        const user = await prisma.user.create({
+            data: {
+                name: 'Rich',
+                email: 'hello@prisma.com',
+            },
+        });
 
-    const post = await prisma.post.create({
-        data:{
-            title : 'My fist post',
-            body : 'My first body'
-        }
-    })
+        // Create a new post associated with the user
+        const post = await prisma.post.create({
+            data: {
+                title: 'My new post',
+                body: 'This is the content of my new post.',
+                slug: 'my-new-post',
+                authorId: user.id,
+            },
+        });
 
-    console.log(post);
+        console.log('Created post:', post);
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
 main();
